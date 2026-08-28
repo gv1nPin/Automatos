5cf1992fb026e1f74cac5b0f7c3346ed520cf924
Hew_Maan <gvinpin.93@gmail.com>
2026-08-28 22:23:08 +0300

    Add abstract class User

---
+++ Main/class_User.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from enums import VeteranRole, BenefitLevel
+
+class User(ABC):
+    def __init__(self,user_id: str,  name: str, clearance_level: int, is_active: bool = True ):
+        # Блок идентификации
+        self.user_id = user_id
+        self.name = name
+        self.clearance_level = clearance_level
+        # Блок контроля
+        self.is_active = is_active
+        self.is_blocked = False
+        self.is_block_reason = ?
+
+    @abstractmethod
+    def get_max_daily_allowed(self, danger_level: int) ->int:
+        pass
