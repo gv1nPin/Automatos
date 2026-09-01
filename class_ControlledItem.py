6f0f63ddd461b8964fa30a98d3febfb53b910aa9
Ankay79 <sledovatelkaluga@yandex.ru>
2026-09-01 19:56:47 +0300

    Add files via upload
    New 01/09

--- class_ControlledItem.py
+++ class_ControlledItem.py
@@ -1,7 +1,16 @@
 
-class Drugs:
-    def __init__(self, drugs_legal_position:str, drugs_licensed_position:str, id_drugs_legal: int, id_drugs_licensed: int, licensed: bool, unit:int, level_clearance ) -> None:
-        if not id_drugs_legal or not id_drugs_licensed:
+class ControlledItem:
+    def __init__(
+        self, 
+         drugs_legal_position:str, 
+         drugs_licensed_position:str, 
+         id_drugs_legal: int|None, 
+         id_drugs_licensed: int|None, 
+         licensed: bool, 
+         unit:int, 
+         level_clearance:int,
+    ) -> None:
+        if id_drugs_legal is None or id_drugs_licensed is None:
             raise ValueError ("ID для всех категорий лекарств должен быть указан!")
         self.drugs_legal_position = drugs_legal_position
         self.drugs_licensed_position = drugs_licensed_position
@@ -10,16 +19,17 @@ class Drugs:
         self.licensed = licensed
         self.unit = unit
         self.level_clearance = level_clearance
-        self.history_give_drugs = []
+        self.history_give_drugs: list = []
         
-    def access (self, user_clearance) -> bool:
+    def access (self, user_clearance:int) -> bool:
     # допустима свободная выдача/выдача по рецепту
         return user_clearance >= self.level_clearance
 
     def control_line (self) -> bool:
         # контроль оборота рецептурных лекарств
-        return self.level_clearance and self.licensed
+        return  self.licensed
 
     def quantity (self, unit) -> str:
         if unit <= 0:
             raise ValueError ("Лекарство отсутствует!")
+        return f"Доступно {unit} шт."
